a3534dd87c808d27c0252ac20b0ba4cde242fe98
asafc64 <asafc64@gmail.com>
2019-08-18 14:36:43 +0300

    Update azure.sh

--- production/azure.sh
+++ production/azure.sh
@@ -82,8 +82,6 @@ TenantId=$(az account show --query tenantId -o tsv)
 echo -e "Creating AD application for CloudShell Colony"
 az ad sp create-for-rbac -n $AppName --password $AppKey --subscription $SubscriptionId ||  quit_on_err "The user that runs the script should be an Owner."
 az ad sp credential reset -n $AppName --password $AppKey --end-date '2299-12-31' --subscription $SubscriptionId 
-
-
 AppId=$(az ad app list --subscription $SubscriptionId --display-name $AppName | jq '.[0].appId' | tr -d \")
 
 echo -e "Configuring access to Azure API"
